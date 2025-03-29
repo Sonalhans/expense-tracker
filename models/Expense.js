a2050adae98ba3c0ec672b31a1c22496b03a7325
Sonal Hans <sonalhans05@gmail.com>
2025-03-29 15:13:28 +0530

    initial commit

---
+++ models/Expense.js
@@ -0,0 +1,8 @@
+const expenseSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  category: String,
+  amount: Number,
+  date: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Expense", expenseSchema);
